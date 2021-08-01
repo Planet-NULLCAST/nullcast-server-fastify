50ff99af42ac79465a680c92b657912246d42339
thealpha93 <tmaakash143@gmail.com>
2021-08-02 00:02:21 +0530

    feat: Initialize tables Script to set up all the tables untrack .env

--- initialize-tables.js
+++ initialize-tables.js
@@ -1,54 +1,101 @@
-const {Client} = require('pg');
+// @ts-nocheck
+const fs = require("fs");
+const { Client } = require("pg");
 
-const client = new Client(
-    {
-        user: process.env.PG_USER,
-        password: process.env.PG_PASSWORD,
-        host: process.env.PG_HOST,
-        database: process.env.PG_DATABSE,
-        // @ts-ignore
-        port: parseInt(process.env.PG_PORT,10)
-    }
-)
+require("dotenv").config();
+
+const client = new Client({
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABSE,
+  port: Number(process.env.PG_PORT),
+});
+
+const superAdminQueries = [
+  `CREATE SCHEMA IF NOT EXISTS public`,
+  `CREATE EXTENSION IF NOT EXISTS pgcrypto`,
+  `GRANT ALL ON schema public TO ${client.user}`,
+];
 
 /**
- * @param {Client} client
+ * Process the sql queries in a file.
+ *
+ * @param {String} fileName full path of the sql file
+ * @returns {Promise}
  */
-async function createUsers(client) {
+async function processSQLFile(fileName) {
+  // Extract SQL queries from files. Assumes no ';' in the fileNames
+  const queries = fs
+    .readFileSync(fileName)
+    .toString()
+    .replace(/(\r\n|\n|\r)/gm, " ") // remove newlines
+    .replace(/\s+/g, " ") // excess white space
+    .split(";") // split into all statements
+    .map(Function.prototype.call, String.prototype.trim)
+    .filter(function (el) {
+      return el.length != 0;
+    }); // remove any empty ones
 
-    await client.query(
-        `CREATE  TABLE IF NOT EXISTS users (
-            id integer GENERATED ALWAYS AS IDENTITY (MINVALUE 10000000 START WITH 10000000 CACHE 200) PRIMARY KEY, 
-            user_name varchar(64) NOT NULL UNIQUE,
-            full_name varchar(64) NOT NULL,
-            email varchar(64) NOT NULL UNIQUE,
-            password varchar(64) NOT NULL,
-            created_at timestamp NOT NULL,
-            updated_at timestamp NOT NULL,
-            cover_image varchar(512),
-            bio varchar(1024),
-            status varchar(10)
-        );`
-    )
+  queries.unshift(...superAdminQueries);
+
+  for await (const query of queries) {
+    await new Promise(function (resolve, reject) {
+      execQueries(query, resolve);
+    });
+  }
+}
+
+/**
+ * Execute the sql statements using pg
+ *
+ * @param {String} query An SQL query
+ * @param {Promise} done
+ */
+async function execQueries(query, done) {
+  if (query.indexOf("COPY") === 0) {
+      
+    // COPY - needs special treatment
+    const regexp = /COPY\ (.*)\ FROM\ (.*)\ DELIMITERS/gim;
+    const matches = regexp.exec(query);
+    const table = matches[1];
+    const fileName = matches[2];
+    const copyString =
+      "COPY " + table + " FROM STDIN DELIMITERS ',' CSV HEADER";
+    const stream = client.copyFrom(copyString);
+
+    stream.on("close", function () {
+      done();
+    });
+
+    const csvFile = __dirname + "/" + fileName;
+    const str = fs.readFileSync(csvFile);
+
+    stream.write(str);
+    stream.end();
+  } else {
+    // Other queries don't need special treatment
+    query = query + ";";
+    const res = await client.query(query);
+    done();
+  }
 }
 
 async function InitializeTables() {
-    try {
-        
-        await client.connect();
-        
-        await createUsers(client);
-        console.log('users table created');
+  try {
+    await client.connect();
 
-        //close connection
-        await client.end();
-        process.exit(0);
-    } catch(error) {
-        client.end();
-        console.error(error);
-        process.exit(1)
-    }
+    await processSQLFile(`${__dirname}/db.sql`);
+    console.log("tables created");
 
+    //close connection
+    await client.end();
+    process.exit(0);
+  } catch (error) {
+    client.end();
+    console.error(error);
+    process.exit(1);
+  }
 }
 
 InitializeTables();
