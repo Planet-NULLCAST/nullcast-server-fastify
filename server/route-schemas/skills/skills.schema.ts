00f44698e99824e4f1ed12f4832a76da49075ff2
Ashwin Chandran <ashwinchandran13@gmail.com>
2021-08-10 20:39:32 +0530

    schemas and refs

---
+++ server/route-schemas/skills/skills.schema.ts
@@ -0,0 +1,34 @@
+import { FastifySchema, RequestBodyDefault } from "fastify";
+
+const skillsRequestBody:RequestBodyDefault = {
+    id: 'https://example.com/schemas/skills',
+    type: 'object',
+    required: ['name', 'user_id'],
+    properties: {
+        name: {
+            type: 'string',
+            description: 'Name of the skill'
+        },
+        user_id: { $ref: 'http://example.com/schemas/user#/properties' }
+    }
+}
+
+export const createSkillsSchema: FastifySchema = {
+    description: 'Create skills',
+    tags: ['user', 'skills'],
+    body: skillsRequestBody,
+    response: {
+        200: {
+            description: 'Skills created'
+        },
+        400: {
+            description: 'Bad request',
+            type: 'object',
+            properties: {
+                message: {
+                    type: 'string'
+                }
+            }
+        }
+    }
+}
