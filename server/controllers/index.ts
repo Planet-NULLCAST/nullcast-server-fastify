090989c3b768f487afe2a81ee8a9a98f55eb4f55
thealpha93 <42433480+thealpha93@users.noreply.github.com>
2021-08-12 12:12:17 +0530

    Revert "change: add more db handler services"

--- server/controllers/index.ts
+++
@@ -1,9 +0,0 @@
-import {createUserController, deleteUserController, getUserController, updateUserController, validateUserController} from './users/users.controller'
-
-export default {
-    createUserController,
-    getUserController,
-    deleteUserController,
-    updateUserController,
-    validateUserController
-}
--- server/plugins/auth.plugin.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance } from 'fastify';
-import auth from 'fastify-auth';
-import authHandler from '../auth/auth.handler';
-
-export function fastifyAuthPlugin(server: FastifyInstance) {
-    server.register(auth).after(()=>{authHandler(server)});
-}
