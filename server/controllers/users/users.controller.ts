3247854f3ba2863513a8fd34fea3d80c0d92143b
thealpha93 <tmaakash143@gmail.com>
2021-08-16 21:03:54 +0530

    fix: fix prepared statment must be unique Removed name from the statements so that they are not cached

--- server/controllers/users/users.controller.ts
+++ server/controllers/users/users.controller.ts
@@ -1,42 +1,48 @@
 import jwt from 'jsonwebtoken';
 
-import {DatabaseHandler} from 'services/postgres/postgres.handler';
+import { DatabaseHandler } from 'services/postgres/postgres.handler';
 import {
-  ValidateUser, User, UpdateUser, DeleteUser
+  ValidateUser,
+  User,
+  UpdateUser,
+  DeleteUser
 } from 'interfaces/user.type';
-import {createHash} from '../../utils/hash-utils';
+import { createHash } from '../../utils/hash-utils';
 
-import {USER_TABLE} from '../../constants/tables';
-
+import {
+  USER_TABLE, ENTITY_TABLE, BADGE_TABLE
+} from '../../constants/tables';
 
 const userHandler = new DatabaseHandler(USER_TABLE);
-// const entityHandler = new DatabaseHandler(ENTITY_TABLE);
-// const badgeHandler = new DatabaseHandler(BADGE_TABLE);
+const entityHandler = new DatabaseHandler(ENTITY_TABLE);
+const badgeHandler = new DatabaseHandler(BADGE_TABLE);
 
-
-export async function createUserController(userData:User): Promise<string> {
+export async function createUserController(userData: User): Promise<string> {
   try {
     // const password = crypto.scryptSync(userData.password as string, process.env.SALT as string,64).toString('hex');
     const hashData = createHash(userData.password as string);
     console.log(hashData);
 
     // Get entity_id and badge_id
-    // const EntityName = "nullcast";
-    // const BadgeName = "noob";
+    const ENTITY_NAME = 'nullcast';
+    const BADGE_NAME = 'noob';
 
-    // const EntityId =  await entityHandler.findOneByField({name:EntityName}, ["id"]);
-    // const BadgeId = await badgeHandler.findOneByField({name:BadgeName}, ["id"]);
+    const entity = await entityHandler.findOneByField({ name: ENTITY_NAME }, [
+      'id'
+    ]);
+    console.log('entity', entity);
+    const badge = await badgeHandler.findOneByField({ name: BADGE_NAME }, [
+      'id'
+    ]);
 
-    const EntityId = 10000000;
-    const BadgeId = 10000000;
-
+    console.log(badge);
 
     const payload: User = {
       ...userData,
-      entity_id: EntityId,
+      entity_id: entity.id,
       salt: hashData.salt,
       password: hashData.password,
-      primary_badge: BadgeId,
+      primary_badge: badge.id,
       slug: userData.slug,
       user_name: userData.user_name.toLowerCase(),
       full_name: userData.full_name.toLowerCase(),
@@ -45,12 +51,15 @@ export async function createUserController(userData:User): Promise<string> {
 
     await userHandler.insertOne(payload);
     // if create success.
-    const token = jwt.sign({userName:payload.user_name, password:hashData.password}, process.env.JWT_KEY as string, {
-      algorithm: 'HS256',
-      expiresIn: parseInt(process.env.JWT_EXPIRY as string, 10)
-    });
+    const token = jwt.sign(
+      { userName: payload.user_name, password: hashData.password },
+      process.env.JWT_KEY as string,
+      {
+        algorithm: 'HS256',
+        expiresIn: parseInt(process.env.JWT_EXPIRY as string, 10)
+      }
+    );
     return token;
-
   } catch (error) {
     throw error;
   }
@@ -58,13 +67,17 @@ export async function createUserController(userData:User): Promise<string> {
 
 export async function getUserController(userName: string): Promise<User> {
   try {
-    return await userHandler.dbHandler<{userName:string}, User>('GET_USER', {userName});
+    return await userHandler.dbHandler<{ userName: string }, User>('GET_USER', {
+      userName
+    });
   } catch (error) {
     throw error;
   }
 }
 
-export async function deleteUserController(userData: DeleteUser): Promise<boolean> {
+export async function deleteUserController(
+  userData: DeleteUser
+): Promise<boolean> {
   try {
     const id = userData?.id as number;
     if (!id) {
@@ -78,7 +91,7 @@ export async function deleteUserController(userData: DeleteUser): Promise<boolea
   }
 }
 
-export async function updateUserController(userData: User):Promise<boolean> {
+export async function updateUserController(userData: User): Promise<boolean> {
   try {
     const id = userData?.id as number;
     if (!id) {
@@ -100,11 +113,15 @@ export async function updateUserController(userData: User):Promise<boolean> {
   }
 }
 
-export async function validateUserController(userData: ValidateUser): Promise<boolean> {
+export async function validateUserController(
+  userData: ValidateUser
+): Promise<boolean> {
   try {
-    return await userHandler.dbHandler<ValidateUser, boolean>('VALIDATE_USER', userData);
+    return await userHandler.dbHandler<ValidateUser, boolean>(
+      'VALIDATE_USER',
+      userData
+    );
   } catch (error) {
     return false;
-
   }
 }
