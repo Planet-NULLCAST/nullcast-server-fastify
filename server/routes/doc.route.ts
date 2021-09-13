5b1cf19b59dcd855feef10bb2fd94a451c8124a6
thealpha93 <tmaakash143@gmail.com>
2021-09-13 22:26:04 +0530

    fix: redoc refresh fails to load add charset = utf-8 and content = text/html remove dist folder and run again.

--- server/routes/doc.route.ts
+++ server/routes/doc.route.ts
@@ -1,17 +1,17 @@
-import {RouteOptions} from 'fastify';
-import {FastifyInstance} from 'fastify/types/instance';
+import { RouteOptions } from 'fastify';
+import { FastifyInstance } from 'fastify/types/instance';
 
-import {docSchema} from 'route-schemas/home/home.schema';
+import { docSchema } from 'route-schemas/home/home.schema';
 
 import * as fs from 'fs';
 import * as path from 'path';
-const stream = fs.createReadStream(path.resolve('./redoc.html'));
 
 const documentation: RouteOptions = {
   method: 'GET',
   url: '/doc',
   schema: docSchema,
   handler: (_, rep) => {
+    const stream = fs.createReadStream(path.resolve('./redoc.html'), 'utf-8');
     rep.type('text/html').send(stream);
   }
 };
