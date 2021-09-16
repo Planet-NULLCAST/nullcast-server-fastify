deccfb6ed4f27c7ff305b254af8afc3fb8678a1a
Dude <mithunraj.s19@gmail.com>
2021-09-16 11:53:18 +0530

    feat: added schema for tags #50

--- server/controllers/tags/tags.controller.ts
+++ server/controllers/tags/tags.controller.ts
@@ -1,32 +1,27 @@
-import { TAGS_STATUS } from 'constants/constants';
-import { TAGS_TABLE, USER_TABLE } from 'constants/tables';
+import { TAGS_STATUS, TAGS_VISIBILITY } from 'constants/constants';
+import { TAGS_TABLE } from 'constants/tables';
+import { UserData } from 'interfaces/auth-token.type';
 import { QueryParams } from 'interfaces/query-params.type';
 import { Tag } from 'interfaces/tags.type';
 import { DatabaseHandler } from 'services/postgres/postgres.handler';
 
 
 const tagsHandler = new DatabaseHandler(TAGS_TABLE);
-const usershandler = new DatabaseHandler(USER_TABLE);
 
-export async function createTagController(payload: Tag) {
+
+export async function createTagController(payload: Tag, userData: UserData) {
   try {
-    // checking if the given id exists
-    const user = await usershandler.findOneById(payload.created_by, ['id']);
-
-    if (!user) {
-      throw { statusCode: 404, message: 'provided user_id not found' };
-    }
 
     const tagData: Tag = {
       ...payload,
       name: payload.name.trim().toLowerCase(),
       slug: payload.name.trim().replace(' ', '_').toLowerCase(),
-      // TODO: Add visibility
-      // visibility: TAGS_VISIBILITY.includes(payload.visibility) ? payload.visibility : 'public',
-      status: TAGS_STATUS.includes(payload.status) ? payload.status : 'active'
+      visibility: TAGS_VISIBILITY.includes(payload.visibility) ? payload.visibility : 'public',
+      status: TAGS_STATUS.includes(payload.status) ? payload.status : 'active',
+      created_by: userData.id
     };
-    await tagsHandler.insertOne<Tag, Tag>(tagData);
-    return true;
+    const tag = await tagsHandler.insertOne<Tag, Tag>(tagData, ['name', 'description', 'meta_title', 'meta_description', 'feature_image', 'slug', 'status', 'created_at', 'updated_at', 'created_by', 'visibility']);
+    return tag.rows[0];
   } catch (error) {
     if (error.code) {
       switch (error.code) {
@@ -41,8 +36,7 @@ export async function createTagController(payload: Tag) {
 export async function getTagController(tagName: string) {
   try {
     const data = { name: tagName };
-    // TODO: Add visibility
-    const tagData = await tagsHandler.findOneByField(data, ['id', 'name', 'description', 'meta_title', 'meta_description', 'feature_image', 'slug', 'status', 'created_at', 'updated_at', 'created_by']);
+    const tagData = await tagsHandler.findOneByField(data, ['id', 'name', 'description', 'meta_title', 'meta_description', 'feature_image', 'slug', 'status', 'created_at', 'updated_at', 'created_by', 'visibility', 'updated_by']);
     if (tagData) {
       return tagData;
     }
@@ -52,15 +46,21 @@ export async function getTagController(tagName: string) {
   }
 }
 
-export async function updateTagController(tagName: string, payload: Tag) {
+export async function updateTagController(tagName: string, payload: Tag, userData: UserData) {
   try {
     const data = { name: tagName };
-    const tagData = await tagsHandler.findOneByField(data, ['id']);
+    const tagData = await tagsHandler.findOneByField(data, ['id', 'created_by']);
 
     if (tagData) {
-      const updatedData = await tagsHandler.updateOneById(tagData.id, payload);
+      const updatedTagData = {
+        ...payload,
+        updated_by: userData.id,
+        created_by: tagData.created_by,
+        updated_at: new Date().toISOString()
+      };
+      const updatedData = await tagsHandler.updateOneById(tagData.id, updatedTagData, ['id', 'name', 'description', 'meta_title', 'meta_description', 'feature_image', 'slug', 'status', 'created_at', 'updated_at', 'created_by', 'visibility', 'updated_by']);
       if (updatedData) {
-        return true;
+        return updatedData.rows[0];
       }
     }
     throw { statusCode: 404, message: 'tag update failed' };
@@ -70,7 +70,7 @@ export async function updateTagController(tagName: string, payload: Tag) {
   }
 }
 
-export async function getTagsController(qParam:QueryParams) {
+export async function getTagsController(qParam: QueryParams) {
   try {
     const tags: Tag[] = await tagsHandler.dbHandler('GET_TAGS', qParam);
     return tags;
