2a40f48108400c6a31488e94cd48fc0d1458bb29
misuvii <midhun.m@neoito.com>
2021-10-11 14:33:01 +0530

    fix: auth changes
    Cookies set as httpOnly & secure, setup logout route, domain from env for production

--- server/routes/auth/auth.route.ts
+++ server/routes/auth/auth.route.ts
@@ -2,7 +2,7 @@ import * as controller from '../../controllers/index';
 import { RouteOptions } from 'fastify';
 import { FastifyInstance } from 'fastify/types/instance';
 import { ValidateUser } from 'interfaces/user.type';
-import { signInSchema } from '../../route-schemas/auth/auth.schema';
+import { logoutSchema, signInSchema } from '../../route-schemas/auth/auth.schema';
 
 const getNewToken: RouteOptions = {
   method: 'GET',
@@ -14,7 +14,7 @@ const getNewToken: RouteOptions = {
       if (userToken) {
         reply.setCookie('token',
           userToken,
-          { signed: false, domain: 'localhost', path: '/', secure: false, httpOnly: false, maxAge: 86400, sameSite: 'none' });
+          { signed: false, domain: '', path: '/', secure: true, httpOnly: true, maxAge: 86400, sameSite: 'none' });
         reply.code(200).send();
       }
     }
@@ -30,16 +30,27 @@ const signIn: RouteOptions = {
     const userData = await controller.signInUserController(request.body as ValidateUser);
     if (userData?.token) {
       reply.setCookie('token', userData.token,
-        { signed: false, domain: 'localhost', path: '/', secure: false, httpOnly: false, maxAge: 86400, sameSite: 'none' });
+        { signed: false, domain: '', path: '/', secure: true, httpOnly: true, maxAge: 86400, sameSite: 'none' });
       reply.code(200).send({ message: 'User logged in successfully', user: userData.user });
     }
     reply.code(401).send({ message: 'Invalid username or password' });
   }
 };
 
+const logOut: RouteOptions = {
+  method: 'POST',
+  url: '/logout',
+  schema: logoutSchema,
+  handler: async(_request, reply) => {
+    reply.setCookie('token', '', { signed: false, domain: '', path: '/', secure: true, httpOnly: true, maxAge: 0, sameSite: 'none' });
+    reply.code(200).send({ message: 'User logged out successfully' });
+  }
+};
+
 function initTokensRoutes(server: FastifyInstance, _: any, done: () => void) {
   server.route(getNewToken);
   server.route(signIn);
+  server.route(logOut);
 
   done();
 }
