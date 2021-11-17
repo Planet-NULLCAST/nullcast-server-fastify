96f4947f9b8b4d452a0855b6ce919baac741a956
thealpha93 <tmaakash143@gmail.com>
2021-11-17 21:51:36 +0530

    chore: refactor models/index.js remove commented out code

--- scripts/migrate/models/index.ts
+++ scripts/migrate/models/index.ts
@@ -5,19 +5,3 @@ export { default as post } from './post.model';
 export { default as subscriber } from './subscriber.model';
 export { default as tag } from './tag.model';
 export { default as skill } from './skills.model';
-
-// import mongoose from 'mongoose';
-// mongoose.Promise = global.Promise;
-
-// const db: {[x: string]: any} = {};
-
-// db.mongoose = mongoose;
-// db.user = require('./user.model');
-// db.enrol = require('./enrol.model');
-// db.courses = require('./course.model');
-// db.posts = require('./post.model');
-// db.subscribers = require('./subscriber.model');
-// db.tags = require('./tag.model');
-// db.skills = require('./skills.model');
-
-// export default db;
