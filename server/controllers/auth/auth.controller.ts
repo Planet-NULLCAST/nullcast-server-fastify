3f277aae2b2b6ee895bccd24cd1f051843dac11c
sNkr-10 <gwrisnkr17@gmail.com>
2021-12-17 15:33:50 +0530

    feat: "user-verification on signup(#135)"

--- server/controllers/auth/auth.controller.ts
+++ server/controllers/auth/auth.controller.ts
@@ -21,8 +21,8 @@ export async function signInUserController(userData: ValidateUser) {
     if (!dbData) {
       return;
     }
-    if(!dbData?.email_verified) {
-      throw({statusCode: 401, message: "User not verified"})
+    if (!dbData?.email_verified) {
+      throw ({statusCode: 401, message: 'User not verified'});
     }
 
     if (verifyHash(userData.password, dbData.password)) {
