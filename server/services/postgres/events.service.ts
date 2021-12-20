7fe0b3d4cb34cfb620ab47cc4ad189c76160c65a
sNkr-10 <gwrisnkr17@gmail.com>
2021-12-20 19:45:00 +0530

    fix: "Changes getEvents api"

--- server/services/postgres/events.service.ts
+++ server/services/postgres/events.service.ts
@@ -4,74 +4,77 @@ import { QueryParams } from 'interfaces/query-params.type';
 import { Client, QueryConfig } from 'pg';
 
 export async function getEvents(queryParams: QueryParams) {
-
-  const DEFAULT_FIELDS = ['id', 'title', 'guest_name', 'guest_designation', 'guest_image', 'registration_link', 'guest_bio', 'created_at', 'created_by',
+  try {
+    const DEFAULT_FIELDS = ['id', 'title', 'guest_name', 'guest_designation', 'guest_image', 'registration_link', 'guest_bio', 'created_at', 'created_by',
     'status', 'published_at', 'banner_image', 'updated_at', 'meta_title', 'description', 'location', 'primary_tag', 'event_time'];
 
-  const {
-    limit_fields = DEFAULT_FIELDS,
-    search = '',
-    page = 1,
-    limit = 10,
-    status = '',
-    order = 'ASC',
-    sort_field = 'created_at'
-  } = queryParams;
+    const {
+      limit_fields = DEFAULT_FIELDS,
+      search = '',
+      page = 1,
+      limit = 10,
+      status = '',
+      order = 'ASC',
+      sort_field = 'created_at'
+    } = queryParams;
 
-  let WHERE_CLAUSE = '';
-  const queryValues: any[] = [];
+    let WHERE_CLAUSE = '';
+    const queryValues: any[] = [];
 
-  if (status) {
-    queryValues.push(status);
-    WHERE_CLAUSE = `WHERE events.status = $${queryValues.length}`;
-  }
-
-  if (search) {
-    queryValues.push(`%${search}%`);
     if (status) {
-      WHERE_CLAUSE = `${WHERE_CLAUSE} 
-      AND (events.meta_title LIKE $${queryValues.length} 
-      OR events.meta_description LIKE $${queryValues.length} 
-      OR events.custom_excerpt LIKE $${queryValues.length})`;
-    } else {
-      WHERE_CLAUSE = `WHERE
-      (events.meta_title LIKE $${queryValues.length} 
-      OR events.meta_description LIKE $${queryValues.length} 
-      OR events.custom_excerpt LIKE $${queryValues.length})`;
+      queryValues.push(status);
+      WHERE_CLAUSE = `WHERE events.status = $${queryValues.length}`;
     }
+
+    if (search) {
+      queryValues.push(`%${search}%`);
+      if (status) {
+        WHERE_CLAUSE = `${WHERE_CLAUSE} 
+        AND (events.meta_title LIKE $${queryValues.length} 
+        OR events.meta_description LIKE $${queryValues.length} 
+        OR events.custom_excerpt LIKE $${queryValues.length})`;
+      } else {
+        WHERE_CLAUSE = `WHERE
+        (events.meta_title LIKE $${queryValues.length} 
+        OR events.meta_description LIKE $${queryValues.length} 
+        OR events.custom_excerpt LIKE $${queryValues.length})`;
+      }
+    }
+    queryValues.push(+limit);
+    queryValues.push((page - 1) * +limit);
+
+    const limitFields = limit_fields.map((item) => `events.${item}`);
+
+    const postgresClient: Client = (globalThis as any).postgresClient as Client;
+
+    const getEventsQuery: QueryConfig = {
+      text: `SELECT ${limitFields}
+              FROM ${EVENT_TABLE} AS events
+              ${WHERE_CLAUSE}
+              ORDER BY 
+              events.${sort_field} ${order}
+              LIMIT $${queryValues.length-1}
+              OFFSET $${queryValues.length};`,
+      values: queryValues
+    };
+
+    const getEventsCountQuery: QueryConfig = {
+      text: `SELECT COUNT(events.id)
+              FROM ${EVENT_TABLE} AS events
+              ${WHERE_CLAUSE};`,
+      values: queryValues.slice(0, -2)
+    };
+
+    const eventData = await postgresClient.query<Event>(getEventsQuery);
+    const eventCountData = await postgresClient.query<Event>(getEventsCountQuery);
+
+    if (eventData.rows && eventData.rows.length) {
+      return  { events: eventData.rows, ...eventCountData.rows[0], limit, page };
+    }
+    return null;
+  } catch (err) {
+    throw err;
   }
-  queryValues.push(+limit);
-  queryValues.push((page - 1) * +limit);
-
-  const limitFields = limit_fields.map((item) => `events.${item}`);
-
-  const postgresClient: Client = (globalThis as any).postgresClient as Client;
-
-  const getEventsQuery: QueryConfig = {
-    text: `SELECT ${limitFields}
-            FROM ${EVENT_TABLE} AS events
-            ${WHERE_CLAUSE}
-            ORDER BY 
-            events.${sort_field} ${order}
-            LIMIT $${queryValues.length-1}
-            OFFSET $${queryValues.length};`,
-    values: queryValues
-  };
-
-  const getEventsCountQuery: QueryConfig = {
-    text: `SELECT COUNT(events.id)
-            FROM ${EVENT_TABLE} AS events
-            ${WHERE_CLAUSE};`,
-    values: queryValues.slice(0, -2)
-  };
-
-  const eventData = await postgresClient.query<Event>(getEventsQuery);
-  const eventCountData = await postgresClient.query<Event>(getEventsCountQuery);
-
-  if (eventData.rows && eventData.rows.length) {
-    return  { events: eventData.rows, ...eventCountData.rows[0], limit, page };
-  }
-  throw new Error('Events not found');
 }
 
 export async function getEventsByUserId(payload: {userId: number}, queryParams: QueryParams) {
