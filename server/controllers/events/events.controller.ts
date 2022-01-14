ae40431e1c32aebc467ab36bfa7174e99158cf28
sNkr-10 <gwrisnkr17@gmail.com>
2022-01-14 12:53:38 +0530

    fix: "some changes"

--- server/controllers/events/events.controller.ts
+++ server/controllers/events/events.controller.ts
@@ -33,7 +33,7 @@ export async function createEventController(eventData:Event, userId:number): Pro
       };
 
       const fields = ['id', 'title', 'guest_name', 'guest_designation', 'guest_image', 'registration_link', 'guest_bio', 'created_at', 'created_by',
-        'slug', 'status', 'published_at', 'banner_image', 'updated_at', 'meta_title', 'description', 'location', 'primary_tag', 'event_time'];
+        'slug', 'status', 'published_at', 'banner_image', 'updated_at', 'meta_title', 'description', 'meta_description', 'location', 'primary_tag', 'event_time'];
 
       const data = await eventHandler.insertOne(payload, fields);
       return data.rows[0] as Event;
@@ -48,7 +48,7 @@ export async function createEventController(eventData:Event, userId:number): Pro
 export async function getEventController(eventId:number):Promise<Event> {
   try {
     const fields = ['id', 'title', 'guest_name', 'guest_designation', 'guest_image', 'registration_link', 'guest_bio', 'created_at', 'created_by',
-      'status', 'published_at', 'banner_image', 'updated_at', 'meta_title', 'description', 'location', 'primary_tag', 'event_time'];
+      'status', 'published_at', 'banner_image', 'updated_at', 'meta_title', 'description',, 'meta_description' 'location', 'primary_tag', 'event_time'];
 
     return await eventHandler.findOneById(eventId, fields) as Event;
 
