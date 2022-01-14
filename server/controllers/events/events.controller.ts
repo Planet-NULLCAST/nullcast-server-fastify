4dfe598dbae4475b8c65d6947d4369665e8fe44f
sNkr-10 <gwrisnkr17@gmail.com>
2022-01-14 12:56:25 +0530

    fix: "some fixes"

--- server/controllers/events/events.controller.ts
+++ server/controllers/events/events.controller.ts
@@ -48,7 +48,7 @@ export async function createEventController(eventData:Event, userId:number): Pro
 export async function getEventController(eventId:number):Promise<Event> {
   try {
     const fields = ['id', 'title', 'guest_name', 'guest_designation', 'guest_image', 'registration_link', 'guest_bio', 'created_at', 'created_by',
-      'status', 'published_at', 'banner_image', 'updated_at', 'meta_title', 'description',, 'meta_description' 'location', 'primary_tag', 'event_time'];
+      'status', 'published_at', 'banner_image', 'updated_at', 'meta_title', 'description', 'meta_description', 'location', 'primary_tag', 'event_time'];
 
     return await eventHandler.findOneById(eventId, fields) as Event;
 
